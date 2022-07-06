5c191d42f7e9556fc0d8d9f02b77c23712b297ba
sidizow <dima.chemezow@yandex.ru>
2022-07-06 17:08:01 +0700

    add calendar

--- app/src/main/java/com/example/messanger/presentation/fragment/home/settings/AccountSettings.kt
+++ app/src/main/java/com/example/messanger/presentation/fragment/home/settings/AccountSettings.kt
@@ -1,14 +1,16 @@
 package com.example.messanger.presentation.fragment.home.settings
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.messanger.R
 import com.example.messanger.databinding.FragmentAccountSettingsBinding
 import com.example.messanger.presentation.core.BaseFragment
 import com.redmadrobot.inputmask.MaskedTextChangedListener
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AccountSettings : BaseFragment() {
 
@@ -22,6 +24,7 @@ class AccountSettings : BaseFragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -31,5 +34,31 @@ class AccountSettings : BaseFragment() {
                 binding.editTextPhone
             )
         )
+
+        val cal = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLable(cal)
+        }
+
+        binding.editTextDate.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+
+    private fun updateLable(cal: Calendar?) {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        if (cal != null) {
+            binding.editTextDate.setText(sdf.format(cal.time))
+        }
     }
 }
